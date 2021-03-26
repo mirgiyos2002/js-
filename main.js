5f49f4a9d517b3f01278029cb204ffc32ff03f60
mirgiyos <mirtil12345@gmail.com>
2021-03-26 10:52:33 +0500

    test

---
+++ main.js
@@ -0,0 +1,25 @@
+var test = prompt('ismingizni yozing');
+alert ('salom ' + test);
+console.log('mening ismim ' + test );
+
+var yourAge = +prompt('sizning yoshigiz nechida');
+alert('Mening yoshim ' + yourAge);
+alert(2021 - yourAge);
+console.log('men ' + yourAge + ' yoshdaman' );
+
+
+var dev = prompt(('1' +  '+' + '2' + '=' + '?'));
+alert('misol javobi  '  + dev);
+console.log('misol 1: 1 + 2 = 3 siznimg javobingiz ' + dev  );
+var dev = prompt(('684' +  '-' + '684' + '=' + '?'));
+alert('misol javobi  '  + dev);
+console.log('misol 2: 684 - 684 = 100 siznimg javobingiz ' + dev  );
+var dev = prompt(('15' +  '*' + '8' + '=' + '?'));
+alert('misol javobi  '  + dev);
+console.log('misol 3: 15 / 8 = 120 siznimg javobingiz ' + dev  );
+var dev = prompt(('5' +  '/' + '5' + '=' + '?'));
+alert('misol javobi  '  + dev);
+console.log('misol 4: 5 / 5 = 1 siznimg javobingiz ' + dev  );
+var dev = prompt(('10' +  '%' + '3' + '=' + '?'));
+alert('misol javobi  '  + dev);
+console.log('misol 5: 10 % 3 = 1 siznimg javobingiz ' + dev  );
